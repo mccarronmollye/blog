3e22c31c8166c984ff499466a73f142174b2be3b
Molly McCarron <mccarronmollye@gmail.com>
2019-05-02 10:09:56 -0400

    App shows all posts with respective user.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,11 @@ import jsonplaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async dispatch => {
     const response = await jsonplaceholder.get('/posts')
 
-    dispatch({ type: 'FETCH_POSTS', payload: response})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data})
   };
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonplaceholder.get(`/users/${id}`)
+
+  dispatch({ type: 'FETCH_USER', payload: response.data})
+}
